3fb6c4aa6b93ff021173217610a0d9834b91da7a
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-03-03 22:28:27 +0100

    Create post request for forecast by city name

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
+// Require dependencies
 const express = require('express');
 const bp = require('body-parser');
-
-const port = 3000;
+const request = require('request');
 const app = express();
 
+// Port number
+const port = 3000;
+
+// Dotenv package
 require('dotenv').config();
 
+// Openweathermap api key
+const apiKey = `${process.env.API_KEY}`;
+
+// Middleware
 app.use(express.static('./public'));
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
+
+// Post request for receive data from openweathermap using city name
+app.post('/', (req, res) => {
+
+    // Get city name from request body
+    const city = req.body.city;
+
+    // Create url for chosen city
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    // Request for data using the URL
+    request(url, function (err, response, body) {
+
+        // Create object from data 
+        let forecast = JSON.parse(body);
+        console.log(forecast);
+    });
+});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
